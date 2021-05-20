348c7177cf48e8eee57d21cac0f5c52aefc2c920
Jorim Jaggi <jjaggi@google.com>
2021-05-20 18:35:30 +0200

    Fix race condition between lockNow() and updateLockscreenTimeout
    If updateLockscreenTimeout gets called before the Runnable queued
    from lockNow gets executed, lockNow request will be ignored. Fix
    this by not clearing out the runnable if it's pending lock request.
    
    Test: Switch user, ensure lockscreen comes up
    Bug: 161149543
    Change-Id: Ie486396fd7328edf8ca0912df92524bb82a1fb7f
    (cherry picked from commit 875fa991aac0f3bbd5c66327408ceae60a24a6b3)
    Merged-In: Ie486396fd7328edf8ca0912df92524bb82a1fb7f
    (cherry picked from commit 1fc88c383eb7d59b3eee7b0064a4aa80f3dee3ff)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -657,6 +657,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private volatile int mTopFocusedDisplayId = INVALID_DISPLAY;
 
     private int mPowerButtonSuppressionDelayMillis = POWER_BUTTON_SUPPRESSION_DELAY_DEFAULT_MILLIS;
+    
+    private boolean mLockNowPending = false;
 
     private int mTorchActionMode;
 
@@ -5473,6 +5475,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mKeyguardDelegate.doKeyguardTimeout(options);
                 }
                 mLockScreenTimerActive = false;
+                mLockNowPending = false;
                 options = null;
             }
         }
@@ -5482,7 +5485,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    ScreenLockTimeout mScreenLockTimeout = new ScreenLockTimeout();
+    final ScreenLockTimeout mScreenLockTimeout = new ScreenLockTimeout();
 
     @Override
     public void lockNow(Bundle options) {
@@ -5494,6 +5497,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mScreenLockTimeout.setLockOptions(options);
         }
         mHandler.post(mScreenLockTimeout);
+        synchronized (mScreenLockTimeout) {
+            mLockNowPending = true;
+        }
     }
 
     // TODO (b/113840485): Move this logic to DisplayPolicy when lockscreen supports multi-display.
@@ -5509,6 +5515,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void updateLockScreenTimeout() {
         synchronized (mScreenLockTimeout) {
+            if (mLockNowPending) {
+                Log.w(TAG, "lockNow pending, ignore updating lockscreen timeout");
+                return;
+            }
             final boolean enable = !mAllowLockscreenWhenOnDisplays.isEmpty()
                     && mDefaultDisplayPolicy.isAwake()
                     && mKeyguardDelegate != null && mKeyguardDelegate.isSecure(mCurrentUserId);
